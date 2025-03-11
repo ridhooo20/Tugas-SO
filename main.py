3afdfb3460fdb96f86c376cc4d4cf7f74da77ae5
ridhomulya <githubridho@gmail.com>
2025-03-11 09:25:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 print("Tugas Sistem Operasi")
 print("Kalkulator Sederhana")
 
-a = int(input("masukan angka pertama: "))
-b = int(input("masukan angka kedua: "))
-
-opsi = input("masukan opsi: (+/-): ")
-
-print("Tugas Sistem Operasi")
-print("Kalkulator Sederhana")
-
 a = int(input("Masukkan angka pertama: "))
 b = int(input("Masukkan angka kedua: "))
 
